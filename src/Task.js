d1f3450fa3df634e04c089a6f4e6a01994003e64
bahasaracoglu <bahasaracoglu@gmail.com>
2023-04-27 23:48:35 +0300

    formatDistanceToNow eklendi.

--- src/Task.js
+++ src/Task.js
@@ -1,18 +1,32 @@
+import { formatDistanceToNow } from "date-fns";
+import { tr } from "date-fns/locale";
 import React from "react";
 
 const Task = ({ taskObj, onComplete }) => {
+  const date = new Date(taskObj.deadline);
+  const toNow = formatDistanceToNow(date, { addSuffix: true, locale: tr });
+
+  console.log(toNow);
+
+  console.log(date);
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">
+        son teslim: <span>{toNow}</span>
+      </div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span className="pill" key={p}>
+            {p}
+          </span>
         ))}
       </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      {onComplete && (
+        <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>
+      )}
     </div>
   );
 };
